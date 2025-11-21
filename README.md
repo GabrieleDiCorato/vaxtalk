3f7b3bd889d1e57d0d348c6ee6a50b9d17511d37
Gabriele Di Corato <gabriele.dicorato@gmail.com>
2025-11-21 14:23:18 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vaxtalk
 A chatbot PoC developed as course requirement for a master in AI.
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from vaxtalk!")
+
+
+if __name__ == "__main__":
+    main()
