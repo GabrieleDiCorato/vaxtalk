62fd1f983fd1896b117347cf1a198ff5431b04fe
Gabriele Di Corato <gabriele.dicorato@gmail.com>
2025-11-25 12:46:27 +0100

    Removed stray folder

--- src/config/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Configuration package."""
-
-from vaxtalk.config.config import load_env_variables, get_env_variable, get_env_int, get_env_list
-
-__all__ = ["load_env_variables", "get_env_variable", "get_env_int", "get_env_list"]
